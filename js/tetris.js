aced5e7937ee8461bd97e4f5d5a4e5ba1c11dc29
runandrerun <the.asantiagojr@gmail.com>
2018-11-13 19:49:20 -0500

    Tetris

--- js/tetris.js
+++ js/tetris.js
@@ -5,6 +5,59 @@ document.addEventListener("DOMContentLoaded", (event) => {
   context.scale(20, 20);
 
 
+  arenaSweep = () => {
+      let rowCount = 1;
+      outer: for (let y = arena.length -1; y > 0; --y) {
+          for (let x = 0; x < arena[y].length; ++x) {
+              if (arena[y][x] === 0) {
+                  continue outer;
+              }
+          }
+
+          const row = arena.splice(y, 1)[0].fill(0);
+          arena.unshift(row);
+          ++y;
+
+          player.score += rowCount * 10;
+          rowCount *= 2;
+      }
+  }
+
+  collide = (arena, player) => {
+      const m = player.matrix;
+      const o = player.pos;
+      for (let y = 0; y < m.length; ++y) {
+          for (let x = 0; x < m[y].length; ++x) {
+              if (m[y][x] !== 0 &&
+                 (arena[y + o.y] &&
+                  arena[y + o.y][x + o.x]) !== 0) {
+                  return true;
+              }
+          }
+      }
+      return false;
+  }
+
+
+    rotate = (matrix, dir) => {
+        for (let y = 0; y < matrix.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [
+                    matrix[x][y],
+                    matrix[y][x],
+                ] = [
+                    matrix[y][x],
+                    matrix[x][y],
+                ];
+            }
+        }
+
+        if (dir > 0) {
+            matrix.forEach(row => row.reverse());
+        } else {
+            matrix.reverse();
+        }
+    }
 
   createMatrix = (w, h) => {
       const matrix = [];
@@ -133,7 +186,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
   const arena = createMatrix(12, 20);
-  
+
   const player = {
       pos: {x: 0, y: 0},
       matrix: null,
